f48cd15b7410d3ab96eda398c959b2e5a00679eb
Dylan <user@users-MacBook-Pro-2.local>
2022-05-17 00:34:20 +0100

    updating server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+// require dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+
+// initialise express
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require("./routes"));
+
+// connection to mongoDB database
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/social-network-api",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// mongo log
+mongoose.set("debug", true);
+
+// listen to server
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+
