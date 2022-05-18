bfbd88438e399eeeff7274fcea51acc5430327d5
Dylan <user@users-MacBook-Pro-2.local>
2022-05-18 23:09:05 +0100

    updating user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,4 @@
-rconst { User, Thought } = require("../models");
+const { User, Thought } = require("../models");
 
 // create user controller variable
 
@@ -12,3 +12,24 @@ module.exports = {
         res.status(400).json(err);
       });
     },
+
+    //get User by ID with thoughts
+    getUserById({ params }, res) {
+     User.findOne({ _id: params.id })
+        .populate("thoughts")
+        .populate ("friends")
+        .select("-__v")
+        .then((dbUserData) => {
+          if (!dbUserData) {
+            res.status(404).json({ message: "No user can be found with the provided id"})
+            return;   
+         }   
+          res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        });
+    },
+
+    
