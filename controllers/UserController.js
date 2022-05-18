b1abe3ab06c5f0d0c24a75ac3e19eb74ff595e29
Dylan <user@users-MacBook-Pro-2.local>
2022-05-18 23:34:23 +0100

    updating user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -70,4 +70,16 @@ module.exports = {
             res.json(dbuserData);
           })
           .catch((err) => res.status(400).json(err.message));
-      },
+    },
+
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true})
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this ID!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+           .catch(err => res.json(err))
+    },
