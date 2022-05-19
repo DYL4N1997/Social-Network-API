fe79c7b231017635d1d9450c9c2ef90b6f206954
Dylan <user@users-MacBook-Pro-2.local>
2022-05-20 00:23:24 +0100

    updating thought routes

---
+++ routes/api/ThoughtRoutes.js
@@ -0,0 +1,14 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  // get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .then((thoughtData) => res.json(thoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  
