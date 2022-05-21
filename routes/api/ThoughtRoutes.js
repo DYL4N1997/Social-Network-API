07518700374ed4110d249dc216f119ff0b6982ac
Dylan <user@users-MacBook-Pro-2.local>
2022-05-21 14:09:57 +0100

    updating thought controller

--- routes/api/ThoughtRoutes.js
+++ routes/api/ThoughtRoutes.js
@@ -12,3 +12,19 @@ const {
 } = require("../../controllers/ThoughtController");
 
 const { route } = require("./userRoutes");
+
+// /api/thoughts
+router.route("/").get(getAllThoughts);
+
+// /api/thoughts/userId
+router.route("/:userId").post(createThought);
+
+// /api/thoughts/thoughtId
+router
+  .route("/:thoughtId")
+  .get(getThoughtByID)
+  .put(updateThought)
+  .delete(deleteThought);
+
+///api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
