d97f7eeceaafb3a3ddfa9b9048bec868ea980e02
Dylan <user@users-MacBook-Pro-2.local>
2022-05-21 13:17:20 +0100

    updating thought controller

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -26,4 +26,23 @@ module.exports = {
       });
   },
 
+  createThought({ params, body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        return User.findOneAndUpdate(
+          { _id: params.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
   
