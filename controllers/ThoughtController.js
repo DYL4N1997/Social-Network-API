198b0e0543a3dbf816967ec95b78e8cd6f0d399e
Dylan <user@users-MacBook-Pro-2.local>
2022-05-21 11:14:13 +0100

    updating thought controller

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -4,11 +4,24 @@ module.exports = {
   // get all thoughts
   getAllThoughts(req, res) {
     Thought.find({})
-      .then((thoughtData) => res.json(thoughtData))
+      .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => {
         console.log(err);
-        res.status(400).json(err);
+        res.status(400).json(err.message);
       });
   },
 
-  
+  getThoughtByID({ params }, res) {
+    Thought.findOne({ _id: params.thoughtId })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this ID" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err.message);
+      });
+  },
