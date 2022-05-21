3bb9f3c6b3cdd75c5ef2a1beed56c61b40f38670
Dylan <user@users-MacBook-Pro-2.local>
2022-05-21 13:53:46 +0100

    updating thought controller

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -60,3 +60,24 @@ module.exports = {
     })
     .catch(err => res.json(err.message));
 },
+
+   removeReaction({ params }, res) {
+     Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res
+            .status(404)
+            .json({ message: "No thoughts with the ID selected!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err.message));
+  },
+
+
+
