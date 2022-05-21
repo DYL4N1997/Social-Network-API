ff7aa99486817b1cc411c1c293a7e08703d94ecd
Dylan <user@users-MacBook-Pro-2.local>
2022-05-21 14:04:20 +0100

    updating thought controller

--- routes/api/ThoughtRoutes.js
+++ routes/api/ThoughtRoutes.js
@@ -1,1 +1,14 @@
 
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtByID,
+  createThought,
+  addReaction,
+  removeReaction,
+  updateThought,
+  deleteThought
+} = require("../../controllers/ThoughtController");
+
+const { route } = require("./userRoutes");
